d2e0ed28a9dc856c5d96ed82be59e3d3100e2de3
onurtaskirancom <taskiranonur9@gmail.com>
2023-12-24 19:13:30 +0300

    deploy app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import ThemeToggle from './ThemeToggle';
+import Gallery from './Gallery';
+import SearchForm from './SearchForm';
+
+const App = () => {
+  return (
+    <main>
+      <ThemeToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
